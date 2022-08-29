ee466cf372706377200dc8eec57323e91526f765
Samer A <thedev.samer@gmail.com>
2022-08-29 15:25:57 +0300

    finished building the login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,56 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { Link } from "react-router-dom";
+import config from "config.json";
+
+const routes = config.routes.client;
+
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <Container>
+      <Row>
+        <Col md={5} className="login__bg"></Col>
+
+        <Col
+          md={7}
+          className="d-flex align-items-center justify-content-center flex-direction-column"
+        >
+          <Form className="login-form">
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+              <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+              Login
+            </Button>
+
+            <div className="py-4">
+              <p className="t3e">
+                Don't have an account?{" "}
+                <Link to={routes.register}>Register</Link>
+              </p>
+            </div>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Login;
